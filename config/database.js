ef43543fb28e8313f3cc19e98809e53995380cfe
Simon van der Weele <vanderweelesimon@gmail.com>
2019-09-29 17:55:44 +0200

    updated source

--- config/database.js
+++ config/database.js
@@ -1,10 +1,11 @@
-if (process.env.NODE_ENV === "production") {
+var development = process.env.NODE_ENV !== "production";
+if (!development) {
   module.exports = {
     mongoURI:
       "mongodb+srv://Simon:mypassword123@cluster0-n4iai.gcp.mongodb.net/test?retryWrites=true&w=majority"
   };
-}else{
-    module.exports = {
-        mongoURI:"mongodb://localhost/nodejscrashcoursedb"
-    };
+} else {
+  module.exports = {
+    mongoURI: "mongodb://localhost/nodejscrashcoursedb"
+  };
 }
