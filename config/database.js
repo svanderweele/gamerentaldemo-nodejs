2f26e7b7d24831db06bd5dadec5899c11fb67ac7
Simon van der Weele <vanderweelesimon@gmail.com>
2019-09-29 18:15:49 +0200

    updated source

--- config/database.js
+++ config/database.js
@@ -3,7 +3,7 @@ var development = process.env.NODE_ENV !== 'production';
 if (!development) {
   module.exports = {
     mongoURI:
-      "mongodb://Simon:mypassword123@cluster0-shard-00-00-n4iai.gcp.mongodb.net:27017,cluster0-shard-00-01-n4iai.gcp.mongodb.net:27017,cluster0-shard-00-02-n4iai.gcp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority"
+      "mongodb+srv://Simon:mypassword123@cluster0-n4iai.gcp.mongodb.net/test?retryWrites=true&w=majority"
   };
 } else {
   module.exports = {
